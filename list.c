8dfa2b67a49685f38d02345d170bc875a18f8966
nikitatorop <torop.nikita1@gmail.com>
2022-05-30 08:42:27 +0300

    Add Linked List implementation and initialization

---
+++ list.c
@@ -0,0 +1,45 @@
+//
+// Created by perf3ctstyle on 30.05.2022.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int number;
+    struct node *next;
+} node;
+
+int main(void) {
+    node *list = NULL;
+
+    node *n = malloc(sizeof(node));
+    if (n == NULL) {
+        return 1;
+    }
+    n->number = 1;
+    n->next = NULL;
+
+    list = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL) {
+        free(list);
+        return 1;
+    }
+    n->number = 2;
+    n->next = NULL;
+
+    list->next = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL) {
+        free(list->next);
+        free(list);
+        return 1;
+    }
+    n->number = 3;
+    n->next = NULL;
+    
+    list->next->next = n;
+}
